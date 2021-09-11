6b73b0e536cecec0e8d45cfd39c19d7906e06648
Tien Dat Pham <dat20036@gmail.com>
2021-09-11 11:27:57 +0700

    first slash command

---
+++ Lilia/Commands/Test.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.SlashCommands;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace Lilia.Commands
+{
+    public class Test : ApplicationCommandModule
+    {
+        [SlashCommand("test", "Just testing.")]
+        public async Task TestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(DSharpPlus.InteractionResponseType.DeferredChannelMessageWithSource);
+            await Task.Delay(5000);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("After 5 secs. I am back."));
+        }
+    }
+}
