f3d397b104747ef867aaa236653684443f5a0824
Tien Dat Pham <dat20036@gmail.com>
2021-09-14 21:11:59 +0700

    Add code for database testing.

--- Lilia/Commands/Normal/Test.cs
+++ Lilia/Commands/Normal/Test.cs
@@ -2,6 +2,7 @@
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using Lilia.Database;
+using Lilia.Database.Models;
 using Lilia.Services;
 using System.Threading.Tasks;
 
@@ -25,6 +26,23 @@ namespace Lilia.Commands.Normal
             DiscordMessage msg = await ctx.RespondAsync("Thinking...");
             await Task.Delay(5000);
             await msg.ModifyAsync("After 5 seconds, this message is edited.");
+        }
+
+        [Command("add5")]
+        [Description("Add 5 shards to your inventory.")]
+        public async Task AddFiveAsync(CommandContext ctx)
+        {
+            User user = this._dbCtx.GetOrCreateUserRecord(ctx.Member.Id);
+            user.Shards += 5;
+            await ctx.RespondAsync("Added 5 shards to your inventory");
+        }
+
+        [Command("check")]
+        [Description("Check shards count in your inventory.")]
+        public async Task CheckShardsAsync(CommandContext ctx)
+        {
+            User user = this._dbCtx.GetOrCreateUserRecord(ctx.Member.Id);
+            await ctx.RespondAsync($"You are having {user.Shards} shards left.");
         }
     }
 }
--- Lilia/Commands/Slash/Test.cs
+++ Lilia/Commands/Slash/Test.cs
@@ -2,6 +2,7 @@
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Lilia.Database;
+using Lilia.Database.Models;
 using Lilia.Services;
 using System.Threading.Tasks;
 
@@ -24,6 +25,23 @@ namespace Lilia.Commands.Slash
             await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
             await Task.Delay(5000);
             await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("After 5 seconds, I am here."));
+        }
+
+        [SlashCommand("add5", "Add 5 shards to your inventory.")]
+        public async Task AddFiveAsync(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            User user = this._dbCtx.GetOrCreateUserRecord(ctx.Member.Id);
+            user.Shards += 5;
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Added 5 shards to your inventory"));
+        }
+
+        [SlashCommand("check", "Check shards count in your inventory.")]
+        public async Task CheckShardsAsync(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            User user = this._dbCtx.GetOrCreateUserRecord(ctx.Member.Id);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"You are having {user.Shards} shards left."));
         }
     }
 }
