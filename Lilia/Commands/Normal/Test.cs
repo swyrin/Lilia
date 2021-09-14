434a327c59301dfcb4e1870858505175b39cfc73
Tien Dat Pham <dat20036@gmail.com>
2021-09-14 20:06:59 +0700

    Made a separate command namespace.

---
+++ Lilia/Commands/Normal/Test.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Lilia.Database;
+using Lilia.Services;
+using System.Threading.Tasks;
+
+namespace Lilia.Commands.Normal
+{
+    public class Test : BaseCommandModule
+    {
+        private LiliaClient _client;
+        private LiliaDbContext _dbCtx;
+
+        public Test(LiliaClient client)
+        {
+            this._client = client;
+            this._dbCtx = client.DbCtx;
+        }
+
+        [Command("test")]
+        [Description("Just a test command.")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            DiscordMessage msg = await ctx.RespondAsync("Thinking...");
+            await Task.Delay(5000);
+            await msg.ModifyAsync("After 5 seconds, this message is edited.");
+        }
+    }
+}
---
+++ Lilia/Commands/Slash/Test.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using Lilia.Database;
+using Lilia.Services;
+using System.Threading.Tasks;
+
+namespace Lilia.Commands.Slash
+{
+    public class Test : ApplicationCommandModule
+    {
+        private LiliaClient _client;
+        private LiliaDbContext _dbCtx;
+
+        public Test(LiliaClient client)
+        {
+            this._client = client;
+            this._dbCtx = client.DbCtx;
+        }
+
+        [SlashCommand("test", "Just a test command.")]
+        public async Task TestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            await Task.Delay(5000);
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("After 5 seconds, I am here."));
+        }
+    }
+}
