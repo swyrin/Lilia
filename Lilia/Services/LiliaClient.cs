1f932f35d96333fc8c7234fb3dd350ddd6ba0bd3
Tien Dat Pham <dat20036@gmail.com>
2021-09-15 17:02:32 +0700

    Rewrite configuration system.

--- Lilia/Services/LiliaClient.cs
+++ Lilia/Services/LiliaClient.cs
@@ -1,13 +1,13 @@
-﻿using DotNetEnv;
-using DSharpPlus;
+﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.EventArgs;
 using Lilia.Commands.Slash;
+using Lilia.Commons;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,21 +18,23 @@ namespace Lilia.Services
     {
         public CancellationTokenSource Cts;
         public LiliaDatabase Database;
+        public JsonConfigurations Configurations;
 
         private void InitialSetup()
         {
-            Env.Load();
             this.Cts = new CancellationTokenSource();
             this.Database = new LiliaDatabase();
+            this.Configurations = JsonConfigurationsManager.Configurations;
         }
 
         public async Task Run()
         {
+            JsonConfigurationsManager.EnsureConfigFileGenerated();
             this.InitialSetup();
 
             DiscordClient client = new(new DiscordConfiguration
             {
-                Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+                Token = this.Configurations.Credentials.DiscordToken,
                 TokenType = TokenType.Bot,
                 MinimumLogLevel = LogLevel.Debug
             });
@@ -43,7 +45,7 @@ namespace Lilia.Services
 
             CommandsNextExtension commandsNext = client.UseCommandsNext(new CommandsNextConfiguration
             {
-                StringPrefixes = new[] { "l." },
+                StringPrefixes = this.Configurations.Client.StringPrefixes,
                 DmHelp = true,
                 Services = services
             });
@@ -73,6 +75,15 @@ namespace Lilia.Services
 
         private Task OnReady(DiscordClient sender, ReadyEventArgs e)
         {
+            ClientActivityData activityData = this.Configurations.Client.Activity;
+
+            DiscordActivity activity = new DiscordActivity 
+            {
+                ActivityType = (ActivityType) activityData.Type,
+                Name = activityData.Name
+            };
+
+            sender.UpdateStatusAsync(activity, (UserStatus) activityData.Status);
             sender.Logger.Log(LogLevel.Information, "Client is ready to serve.");
             return Task.CompletedTask;
         }
