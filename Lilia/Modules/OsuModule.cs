274d2e39541d0cbee43f731832f28299a2b160fe
Tien Dat Pham <dat20036@gmail.com>
2021-09-16 10:18:47 +0700

    Command system changes.

--- Lilia/Modules/OsuModule.cs
+++ Lilia/Modules/OsuModule.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Nalia.Modules
+namespace Lilia.Modules
 {
 	[Group("osu")]
 	[Description("Represents for osu! commands (only works in Bancho server, other servers will be supported soon.")]
---
+++ Lilia/Modules/OwnerModule.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Lilia.Database;
+using Lilia.Services;
+using System.Threading.Tasks;
+
+namespace Lilia.Modules
+{
+    public class OwnerModule : BaseCommandModule
+    {
+        private LiliaClient _client;
+        private LiliaDbContext _dbCtx;
+
+        public OwnerModule(LiliaClient client)
+        {
+            this._client = client;
+            this._dbCtx = client.Database.GetContext();
+        }
+
+        [Command("shutdown")]
+        [Description("Shutdown the bot.")]
+        [RequireOwner]
+        public async Task ShutdownCommand(CommandContext ctx)
+        {
+            await ctx.RespondAsync("Hope to see you again.");
+            this._client.Cts.Cancel();
+        }
+    }
+}
