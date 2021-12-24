f44cf28c7ad51312057334d27d6859829c72726a
Tien Dat Pham <dat20036@gmail.com>
2021-12-24 12:25:13 +0700

    something something new?

--- Lilia/Modules/MusicModule.cs
+++ Lilia/Modules/MusicModule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
@@ -11,7 +13,7 @@ namespace Lilia.Modules;
 public class MusicModule : BaseCommandModule
 {
     [Command("join")]
-    [RequireOwner]
+    [Description("Jumps right to the voice channel that you are in.")]
     public async Task JoinVoiceCommand(CommandContext ctx)
     {
         DiscordChannel channel = ctx.Member.VoiceState?.Channel;
@@ -24,28 +26,49 @@ public class MusicModule : BaseCommandModule
 
         LavalinkExtension lavalinkExtension = ctx.Client.GetLavalink();
         LavalinkNodeConnection node = lavalinkExtension.ConnectedNodes.Values.First();
-            
+        LavalinkGuildConnection connection = node.GetGuildConnection(ctx.Guild);
+
+        if (connection != null && connection.Channel == channel && connection.IsConnected)
+        {
+            await ctx.RespondAsync("Already there.");
+            return;
+        }
+
         await node.ConnectAsync(channel);
         await (await ctx.Guild.GetMemberAsync(ctx.Client.CurrentUser.Id)).SetDeafAsync(true, "Self deaf");
         await ctx.RespondAsync("Connected.");
     }
-        
+
     [Command("leave")]
-    [RequireOwner]
+    [Description("Leave currently joined voice channel")]
     public async Task LeaveVoiceCommand(CommandContext ctx)
     {
         LavalinkExtension lavalinkExtension = ctx.Client.GetLavalink();
         LavalinkNodeConnection node = lavalinkExtension.ConnectedNodes.Values.First();
         LavalinkGuildConnection connection = node.GetGuildConnection(ctx.Guild);
-            
+
+        if (!connection.IsConnected)
+        {
+            await ctx.RespondAsync("Already left.");
+            return;
+        }
+
         await connection.DisconnectAsync();
         await ctx.RespondAsync("Disconnected.");
     }
 
-    [Command("radio")]
-    [RequireOwner]
-    public async Task TransmitRadioCommand(CommandContext ctx, string search = "https://listen.moe/stream")
+    [Command("play")]
+    [Aliases("p")]
+    [Description("Play music, that's it. Soundcloud tracks must prepend 'sc-' before the search.")]
+    public async Task PlayCommand(CommandContext ctx,
+        [Description("Text to search, can be an url.")] [RemainingText] string search)
     {
+        if (string.IsNullOrEmpty(search))
+        {
+            await ctx.RespondAsync("Did you forget something?");
+            return;
+        }
+
         DiscordChannel channel = ctx.Member.VoiceState?.Channel;
 
         if (channel == null)
@@ -53,18 +76,62 @@ public class MusicModule : BaseCommandModule
             await ctx.RespondAsync("Join a voice channel please.");
             return;
         }
-            
+
         LavalinkExtension lavalinkExtension = ctx.Client.GetLavalink();
         LavalinkNodeConnection node = lavalinkExtension.ConnectedNodes.Values.First();
         LavalinkGuildConnection connection = node.GetGuildConnection(ctx.Guild);
         LavalinkLoadResult loadResult;
-            
-        if (search.ToLower().Contains("youtube")) loadResult = await node.Rest.GetTracksAsync(search);
-        else if (search.ToLower().Contains("soundcloud")) loadResult = await node.Rest.GetTracksAsync(search, LavalinkSearchType.SoundCloud);
-        else loadResult = await node.Rest.GetTracksAsync(new Uri(search, UriKind.RelativeOrAbsolute));
+
+        bool canConstructUri = Uri.TryCreate(search, UriKind.Absolute, out Uri result);
+        
+        if (canConstructUri) loadResult = await node.Rest.GetTracksAsync(result);
+        else
+        {
+            if (search.ToLower().StartsWith("sc-")) loadResult = await node.Rest.GetTracksAsync($"{search.Replace("sc-", "")}", LavalinkSearchType.SoundCloud);
+            else loadResult = await node.Rest.GetTracksAsync(search);
+        }
 
         LavalinkTrack track = loadResult.Tracks.First();
         await connection.PlayAsync(track);
         await ctx.RespondAsync($"Now playing {track.Uri}");
+    }
+
+    [Command("nowplaying")]
+    [Aliases("np", "now")]
+    [Description("Check currently played track")]
+    public async Task NowPlayingCommand(CommandContext ctx)
+    {
+        LavalinkExtension lavalinkExtension = ctx.Client.GetLavalink();
+        LavalinkNodeConnection node = lavalinkExtension.ConnectedNodes.Values.First();
+        LavalinkGuildConnection connection = node.GetGuildConnection(ctx.Guild);
+
+        LavalinkTrack track = connection.CurrentState.CurrentTrack;
+        
+        if (!connection.IsConnected)
+        {
+            await ctx.RespondAsync("I did not join the voice chat");
+            return;
+        }
+
+        if (connection.CurrentState.CurrentTrack == null)
+        {
+            await ctx.RespondAsync("Not playing anything");
+            return;
+        }
+
+        if (connection.CurrentState.CurrentTrack.IsStream)
+        {
+            await ctx.RespondAsync($"I am playing a stream from: {connection.CurrentState.CurrentTrack.Uri}");
+            return;
+        }
+
+        StringBuilder text = new StringBuilder();
+        text.AppendLine($"{Formatter.Bold("Track")}: {track.Title}");
+        text.AppendLine($"{Formatter.Bold("Author")}: {track.Author}");
+        text.AppendLine($"{Formatter.Bold("Position")}: {connection.CurrentState.PlaybackPosition:g}/{track.Length:g}");
+        text.AppendLine($"{Formatter.Bold("Source")}: {track.Uri}");
+
+        await ctx.RespondAsync(text.ToString());
+
     }
 }
