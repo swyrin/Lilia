6a1848424bb8af9a5202965770fbcbf8235bd24f
Tien Dat Pham <dat20036@gmail.com>
2022-01-01 18:24:30 +0700

    Add editpsa and fix existing commands

--- Lilia/Modules/ModerationModule.cs
+++ Lilia/Modules/ModerationModule.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Exceptions;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.SlashCommands;
@@ -121,19 +122,73 @@ public class ModerationModule : ApplicationCommandModule
         await ctx.FollowUpAsync(builder);
     }
 
-    [SlashCommand("psa", "Send PSAs to a channel without the member knowings the sender")]
-    [SlashRequirePermissions(Permissions.KickMembers)]
+    [SlashCommand("sendpsa", "Send PSA to a channel without the member knowing the sender")]
+    [SlashRequirePermissions(Permissions.ManageGuild)]
     public async Task SendPsaCommand(InteractionContext ctx,
-        [Option("message_id", "Message ID to copy")] string msgId,
+        [Option("message_id", "Message ID to copy, same channel as command")] string msgId,
         [ChannelTypes(ChannelType.Text, ChannelType.News, ChannelType.Store, ChannelType.NewsThread, ChannelType.PublicThread)]
         [Option("channel", "Channel to send")] DiscordChannel channel)
     {
-        await ctx.DeferAsync();
+        try
+        {
+            await ctx.DeferAsync();
+
+            DiscordMessage msg = await ctx.Channel.GetMessageAsync(Convert.ToUInt64(msgId));
+            await channel.SendMessageAsync(msg.Content);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Sent to the destination channel"));
+        }
+        catch (NotFoundException)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Message with specified ID was not found in this channel"));
+        }
+        catch (FormatException)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Invalid message ID"));
+        }
+    }
+    
+    [SlashCommand("editpsa", "Edit an existing PSA")]
+    [SlashRequirePermissions(Permissions.ManageGuild)]
+    public async Task EditPsaCommand(InteractionContext ctx,
+        [Option("old_message_id", "Old message ID")] string msgIdOld,
+        [Option("new_message_id", "New message ID, same channel as command")] string msgIdNew,
+        [ChannelTypes(ChannelType.Text, ChannelType.News, ChannelType.Store, ChannelType.NewsThread, ChannelType.PublicThread)]
+        [Option("channel", "Previously sent PSA channel")] DiscordChannel channel)
+    {
+        try
+        {
+            await ctx.DeferAsync();
         
-        DiscordMessage msg = await ctx.Channel.GetMessageAsync(Convert.ToUInt64(msgId));
-        await channel.SendMessageAsync(msg.Content);
+            DiscordMessage oldMsg = await channel.GetMessageAsync(Convert.ToUInt64(msgIdOld));
+
+            if (oldMsg.Author != ctx.Client.CurrentUser)
+            {
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                    .WithContent("I was not the one to write the PSA"));
+            
+                return;
+            }
         
-        await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-            .WithContent("Sent to the destination channel"));
+            DiscordMessage newMsg = await ctx.Channel.GetMessageAsync(Convert.ToUInt64(msgIdNew));
+
+            await oldMsg.ModifyAsync(newMsg.Content);
+        
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Edited the message"));    
+        }
+        catch (NotFoundException)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Either the old message was not found in the provided channel or the new message was not found in this channel"));
+        }
+        catch (FormatException)
+        {
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent("Invalid message ID(s)"));
+        }
     }
 }
