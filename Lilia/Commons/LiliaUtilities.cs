789e80e3ce989b306dfc82b07f71d411194fa788
Tien Dat Pham <dat20036@gmail.com>
2022-01-24 09:18:42 +0700

    Reduce fragmented embed creation process

---
+++ Lilia/Commons/LiliaUtilities.cs
@@ -0,0 +1,16 @@
+﻿using DSharpPlus.Entities;
+using System;
+
+namespace Lilia.Commons
+{
+    public static class LiliaUtilities
+    {
+        public static DiscordEmbedBuilder GetDefaultEmbedTemplate(DiscordMember member)
+        {
+            return new DiscordEmbedBuilder()
+                .WithTimestamp(DateTime.Now)
+                .WithColor(DiscordColor.Rose)
+                .WithFooter($"Requested by: {member.DisplayName}#{member.Discriminator}", member.AvatarUrl);
+        }
+    }
+}
