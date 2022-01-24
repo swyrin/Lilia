64b1b8d0421dd18a5996b110d8acab2954d3ea2c
Tien Dat Pham <dat20036@gmail.com>
2022-01-24 10:25:33 +0700

    Fix critical bug and clean up

--- Lilia/Modules/ModerationModule.cs
+++ Lilia/Modules/ModerationModule.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
@@ -27,29 +30,29 @@ public class ModerationModule : ApplicationCommandModule
     [SlashCommand("ban", "Ban members, obviously")]
     [SlashRequirePermissions(Permissions.BanMembers)]
     public async Task BanMembersCommand(InteractionContext ctx,
-        [Option("reason", "Reason to ban")] string reason = "")
+        [Option("reason", "Reason to ban")] string reason = "Rule violation")
     {
         await ctx.DeferAsync();
 
-        DiscordMessage msg = await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder()
             .WithContent($"{Formatter.Bold("Mention")} all the people you want to ban"));
 
         InteractivityExtension interactivity = ctx.Client.GetInteractivity();
 
-        var res = await interactivity.WaitForMessageAsync(x => x.MentionedUsers.Any());
+        var res = await interactivity.WaitForMessageAsync(x => x.MentionedUsers.Any(), TimeSpan.FromMinutes(5));
 
         if (res.TimedOut)
         {
             await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
-                .WithContent("Time exceeded"));
+                .WithContent("Timed out"));
 
             return;
         }
         
         await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
             .WithContent("Banning mischievous people"));
-
-        DiscordFollowupMessageBuilder builder = new DiscordFollowupMessageBuilder();
+        
+        StringBuilder stringBuilder = new();
         
         foreach (DiscordUser user in res.Result.MentionedUsers)
         {
@@ -57,40 +60,37 @@ public class ModerationModule : ApplicationCommandModule
             
             if (member == ctx.Member)
             {
-                builder.WithContent("Beaned you");
+                stringBuilder.AppendLine("Beaned you");
             }
             else
             {
-                reason = string.IsNullOrWhiteSpace(reason)
-                    ? $"Banned by {ctx.Member.DisplayName}#{ctx.Member.Discriminator}"
-                    : reason;
-                
-                await ctx.Guild.BanMemberAsync(member, 0, reason);
-                builder.WithContent($"Banned {member.DisplayName}#{member.Discriminator}");
+                await ctx.Guild.BanMemberAsync(member, 0, $"Banned by {ctx.Member.DisplayName}#{ctx.Member.Discriminator} -> Reason: {reason}");
+                stringBuilder.AppendLine($"Banned {member.DisplayName}#{member.Discriminator}");
             }
         }
 
-        await ctx.FollowUpAsync(builder);
+        await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
+            .WithContent(stringBuilder.ToString()));
     }
 
     [SlashCommand("kick", "Kick members, obviously")]
     [SlashRequirePermissions(Permissions.KickMembers)]
     public async Task KickMembersCommand(InteractionContext ctx,
-        [Option("reason", "Reason to kick")] string reason = "")
+        [Option("reason", "Reason to kick")] string reason = "Rule violation")
     {
         await ctx.DeferAsync();
 
-        DiscordMessage msg = await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder()
             .WithContent($"{Formatter.Bold("Mention")} all the people you want to kick"));
 
         InteractivityExtension interactivity = ctx.Client.GetInteractivity();
 
-        var res = await interactivity.WaitForMessageAsync(x => x.MentionedUsers.Any());
+        var res = await interactivity.WaitForMessageAsync(x => x.MentionedUsers.Any(), TimeSpan.FromMinutes(5));
 
         if (res.TimedOut)
         {
             await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
-                .WithContent("Time exceeded"));
+                .WithContent("Timed out"));
 
             return;
         }
@@ -98,7 +98,7 @@ public class ModerationModule : ApplicationCommandModule
         await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
             .WithContent("Kicking mischievous people"));
 
-        DiscordFollowupMessageBuilder builder = new DiscordFollowupMessageBuilder();
+        StringBuilder stringBuilder = new();
         
         foreach (DiscordUser user in res.Result.MentionedUsers)
         {
@@ -106,23 +106,20 @@ public class ModerationModule : ApplicationCommandModule
             
             if (member == ctx.Member)
             {
-                builder.WithContent("Imagine kicking yourself, smh");
+                stringBuilder.AppendLine("Skipped you");
             }
             else
             {
-                reason = string.IsNullOrWhiteSpace(reason)
-                    ? $"Kicked by {ctx.Member.DisplayName}#{ctx.Member.Discriminator}"
-                    : reason;
-
-                await member.RemoveAsync(reason);
-                builder.WithContent($"Kicked {member.DisplayName}#{member.Discriminator}");
+                await ctx.Guild.BanMemberAsync(member, 0, $"Kicked by {ctx.Member.DisplayName}#{ctx.Member.Discriminator} -> Reason: {reason}");
+                stringBuilder.AppendLine($"Kicked {member.DisplayName}#{member.Discriminator}");
             }
         }
 
-        await ctx.FollowUpAsync(builder);
+        await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
+            .WithContent(stringBuilder.ToString()));
     }
 
-    [SlashCommand("sendpsa", "Send PSA to a channel without the member knowing the sender")]
+    [SlashCommand("sendpsa", "Send PSA to a channel")]
     [SlashRequirePermissions(Permissions.ManageGuild)]
     public async Task SendPsaCommand(InteractionContext ctx,
         [Option("message_id", "Message ID to copy, same channel as command")] string msgId,
@@ -139,56 +136,113 @@ public class ModerationModule : ApplicationCommandModule
             await ctx.EditResponseAsync(new DiscordWebhookBuilder()
                 .WithContent("Sent to the destination channel"));
         }
-        catch (NotFoundException)
+        catch (Exception ex)
         {
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Message with specified ID was not found in this channel"));
-        }
-        catch (FormatException)
-        {
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Invalid message ID"));
+            switch (ex)
+            {
+                case NotFoundException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent($"I can not find a message with {msgId} in this channel"));    
+                    break;
+                case FormatException:
+                case OverflowException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Invalid ID(s) provided"));
+                    break;
+                default:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Unknown error found"));
+                    break;
+            }
         }
     }
     
     [SlashCommand("editpsa", "Edit an existing PSA")]
     [SlashRequirePermissions(Permissions.ManageGuild)]
     public async Task EditPsaCommand(InteractionContext ctx,
-        [Option("old_message_id", "Old message ID")] string msgIdOld,
-        [Option("new_message_id", "New message ID, same channel as command")] string msgIdNew,
         [ChannelTypes(ChannelType.Text, ChannelType.News, ChannelType.Store, ChannelType.NewsThread, ChannelType.PublicThread)]
-        [Option("channel", "Previously sent PSA channel")] DiscordChannel channel)
+        [Option("channel", "Where the old PSA is is sent")] DiscordChannel channel,
+        [Option("old_message_id", "Old message ID")] string msgIdOld,
+        [Option("new_message_id", "New message ID, same channel as command")] string msgIdNew
+        )
     {
         try
         {
             await ctx.DeferAsync();
-        
-            DiscordMessage oldMsg = await channel.GetMessageAsync(Convert.ToUInt64(msgIdOld));
 
-            if (oldMsg.Author != ctx.Client.CurrentUser)
-            {
-                await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                    .WithContent("I was not the one to write the PSA"));
-            
-                return;
-            }
-        
+            DiscordMessage oldMsg = await channel.GetMessageAsync(Convert.ToUInt64(msgIdOld));
             DiscordMessage newMsg = await ctx.Channel.GetMessageAsync(Convert.ToUInt64(msgIdNew));
 
             await oldMsg.ModifyAsync(newMsg.Content);
-        
+
             await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Edited the message"));    
+                .WithContent("Edited the PSA"));
         }
-        catch (NotFoundException)
+        catch (Exception ex)
         {
-            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Either the old message was not found in the provided channel or the new message was not found in this channel"));
+            switch (ex)
+            {
+                case NotFoundException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent($"I can not find a message with ID {msgIdNew} in this channel or {msgIdOld} in provided channel"));    
+                    break;
+                case FormatException:
+                case OverflowException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Invalid ID(s) provided"));
+                    break;
+                case UnauthorizedException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("I was not the one to write the PSA"));
+                    break;
+                default:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Unknown error found"));
+                    break;
+            }
         }
-        catch (FormatException)
+    }
+    
+    [SlashCommand("copypsa", "Copy a PSA sent by me (with formats)")]
+    [SlashRequirePermissions(Permissions.ManageGuild)]
+    public async Task CopyPsaCommand(InteractionContext ctx,
+        [ChannelTypes(ChannelType.Text, ChannelType.News, ChannelType.Store, ChannelType.NewsThread, ChannelType.PublicThread)]
+        [Option("channel", "Where the PSA is sent")] DiscordChannel channel,
+        [Option("message_id", "Message ID to copy")] string msgId)
+    {
+        try
         {
+            await ctx.DeferAsync();
+            
+            ulong msgIdU = Convert.ToUInt64(msgId); 
+            
+            DiscordMessage msg = await channel.GetMessageAsync(msgIdU);
+
             await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Invalid message ID(s)"));
+                .WithContent(msg.Content));
+        }
+        catch (Exception ex)
+        {
+            switch (ex)
+            {
+                case NotFoundException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent($"I can not find a message with {msgId} in provided channel"));
+                    break;
+                case FormatException:
+                case OverflowException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Invalid ID(s) provided"));
+                    break;
+                case UnauthorizedException:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("I was not the one to write the PSA"));
+                    break;
+                default:
+                    await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                        .WithContent("Unknown error found"));
+                    break;
+            }
         }
     }
 }
