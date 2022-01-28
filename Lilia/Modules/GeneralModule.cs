c07a07da57bbef7b2cc852938b74fcc2228f12e6
Tien Dat Pham <dat20036@gmail.com>
2022-01-28 16:59:44 +0700

    Add "changes" command

--- Lilia/Modules/GeneralModule.cs
+++ Lilia/Modules/GeneralModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Lilia.Commons;
@@ -25,6 +26,20 @@ public class GeneralModule : ApplicationCommandModule
             .WithTitle("My uptime")
             .AddField("Uptime", $"{DateTime.Now.Subtract(this._client.StartTime):g}", true)
             .AddField("Start since", this._client.StartTime.ToLongDateString(), true);
+
+        await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+            .AddEmbed(embedBuilder.Build()));
+    }
+    
+    [SlashCommand("changes", "What's new in this version of \"me\"")]
+    public async Task ViewChangelogsCommand(InteractionContext ctx)
+    {
+        await ctx.DeferAsync(true);
+
+        DiscordEmbedBuilder embedBuilder = LiliaUtilities.GetDefaultEmbedTemplate(ctx.Member)
+            .WithTitle("Here is the changelogs")
+            .AddField("All versions", Formatter.MaskedUrl("Click me!", new Uri("https://github.com/Swyreee/Lilia/blob/master/CHANGELOGS.md") , "A GitHub link"), true)
+            .AddField("Development version", Formatter.MaskedUrl("Click me!", new Uri("https://github.com/Swyreee/Lilia/blob/master/CHANGELOG.md"), "A GitHub link"), true);
 
         await ctx.EditResponseAsync(new DiscordWebhookBuilder()
             .AddEmbed(embedBuilder.Build()));
