2b179431fac8d735f5e16a7a3e505d8d92bb59e2
Tien Dat Pham <dat20036@gmail.com>
2022-02-02 10:19:54 +0700

    Remove formatting utils

--- Lilia/Commons/LiliaUtilities.cs
+++ Lilia/Commons/LiliaUtilities.cs
@@ -1,6 +1,5 @@
 ﻿using DSharpPlus.Entities;
 using System;
-using DSharpPlus;
 
 namespace Lilia.Commons
 {
@@ -13,11 +12,5 @@ namespace Lilia.Commons
                 .WithColor(DiscordColor.Rose)
                 .WithFooter($"Requested by: {member.DisplayName}#{member.Discriminator}", member.AvatarUrl);
         }
-
-        public static string ToDiscordBold(this string str) => Formatter.Bold(str);
-        public static string ToDiscordItalic(this string str) => Formatter.Italic(str);
-        public static string ToDiscordUnderline(this string str) => Formatter.Underline(str);
-        public static string ToDiscordEscapedFormat(this string str) => Formatter.Sanitize(str);
-        public static string ToDiscordNoFormat(this string str) => Formatter.Strip(str);
     }
 }
