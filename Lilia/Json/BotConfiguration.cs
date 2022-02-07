8a7600a09d96e3c5507aee4dc0a691f495b5922d
Tien Dat Pham <dat20036@gmail.com>
2022-02-07 17:35:51 +0700

    Forgot to add this into JSON changes

---
+++ Lilia/Json/BotConfiguration.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Lilia.Json;
+
+public class BotConfiguration : BaseJson
+{
+    public BotConfiguration()
+    {
+        FilePath = "config.json";
+    }
+
+    [JsonProperty("client")] public ClientData Client = new();
+
+    [JsonProperty("credentials")] public CredentialsData Credentials = new();
+
+    [JsonProperty("lavalink")] public LavalinkData Lavalink = new();
+}
+
+public class ClientData
+{
+    [JsonProperty("private_guilds")] public List<ulong> PrivateGuildIds = new();
+
+    [JsonProperty("slash_commands_for_global")] public bool SlashCommandsForGlobal = true;
+    
+    [JsonProperty("activity")] public ClientActivityData Activity = new();
+}
+
+public class ClientActivityData
+{
+    [JsonProperty("type")] public string Type = "Watching";
+
+    [JsonProperty("name")] public string Name = "you";
+
+    [JsonProperty("status")] public string Status = "DoNotDisturb";
+}
+
+public class CredentialsData
+{
+    [JsonProperty("discord_token")] public string DiscordToken;
+
+    [JsonProperty("db_password")] public string DbPassword = "thisisliterallynotapassword";
+
+    [JsonProperty("osu")] public OsuData Osu = new();
+}
+
+public class OsuData
+{
+    [JsonProperty("client_id")] public long ClientId;
+    
+    [JsonProperty("client_secret")] public string ClientSecret;
+}
+
+public class LavalinkData
+{
+    [JsonProperty("host")] public string Hostname = "localhost";
+
+    [JsonProperty("port")] public int Port = 2333;
+
+    [JsonProperty("password")] public string Password = "youshallnotpass";
+}
