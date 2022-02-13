b91e6456549305d9855c7e580e8f39d7b4b8b182
Tien Dat Pham <dat20036@gmail.com>
2022-02-13 20:06:26 +0700

    File separated by ReSharper:tm:

---
+++ Helya/Database/HelyaDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Helya.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Helya.Database;
+
+public class HelyaDatabaseContext : DbContext
+{
+    public HelyaDatabaseContext(DbContextOptions<HelyaDatabaseContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DbGuild> Guilds { get; set; }
+    public DbSet<DbUser> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        base.OnConfiguring(options);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Helya/Database/HelyaDatabaseContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Helya.Commons;
+using Helya.Json;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace Helya.Database;
+
+public class HelyaDatabaseContextFactory : IDesignTimeDbContextFactory<HelyaDatabaseContext>
+{
+    public HelyaDatabaseContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<HelyaDatabaseContext>();
+        var connStringBuilder = new SqliteConnectionStringBuilder
+        {
+            DataSource = "database.db",
+            Password = JsonManager<BotConfiguration>.Read().Credentials.DbPassword
+        };
+
+        optionsBuilder
+#if DEBUG
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors()
+#endif
+            .UseLoggerFactory(new SerilogLoggerFactory(Log.Logger))
+            .UseSqlite(connStringBuilder.ToString());
+        
+        var ctx = new HelyaDatabaseContext(optionsBuilder.Options);
+        ctx.Database.SetCommandTimeout(30);
+        return ctx;
+    }
+}
