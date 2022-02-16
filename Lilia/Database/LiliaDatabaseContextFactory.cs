3d4c11bcda29da282ff0ee32950b048720c02df4
Tien Dat Pham <dat20036@gmail.com>
2022-02-16 13:40:46 +0700

    Reduce code repetition

--- Lilia/Database/LiliaDatabaseContextFactory.cs
+++ Lilia/Database/LiliaDatabaseContextFactory.cs
@@ -1,8 +1,6 @@
 ﻿using Lilia.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Serilog;
-using Serilog.Extensions.Logging;
 
 namespace Lilia.Database;
 
@@ -10,22 +8,7 @@ public class HelyaDatabaseContextFactory : IDesignTimeDbContextFactory<LiliaData
 {
     public LiliaDatabaseContext CreateDbContext(string[] args)
     {
-        var optionsBuilder = new DbContextOptionsBuilder<HelyaDatabaseContext>();
-        var connStringBuilder = new SqliteConnectionStringBuilder
-        {
-            DataSource = "database.db",
-            Password = JsonManager<BotConfiguration>.Read().Credentials.DbPassword
-        };
-
-        optionsBuilder
-#if DEBUG
-            .EnableSensitiveDataLogging()
-            .EnableDetailedErrors()
-            .UseLoggerFactory(new SerilogLoggerFactory(Log.Logger))
-#endif
-            .UseSqlite(connStringBuilder.ToString());
-
-        var ctx = new HelyaDatabaseContext(optionsBuilder.Options);
+        var ctx = new LiliaDatabaseContext(LiliaClient.OptionsBuilder.Options);
         ctx.Database.SetCommandTimeout(30);
         return ctx;
     }
