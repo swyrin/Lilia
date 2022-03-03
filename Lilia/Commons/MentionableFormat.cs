8518229beb8b9b1f42412d0eb25fd024d8e2d18a
Tien Dat Pham <dat20036@gmail.com>
2022-03-03 21:36:27 +0700

    Add mention formatting for Discord mentionable entities

---
+++ Lilia/Commons/MentionableFormat.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+
+namespace Lilia.Commons;
+
+public static class MentionableFormat
+{
+    public static string GetMention(this IChannel channel)
+    {
+        return $"<#{channel.Id}>";
+    }
+    
+    public static string GetMention(this IUser user)
+    {
+        return $"<@{user.Id}>";
+    }
+    
+    public static string GetMention(this IRole role)
+    {
+        return $"<@&{role.Id}>";
+    }
+}
