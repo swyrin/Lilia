ed430376de45b90388222429acf15f4e28c9d23d
Tien Dat Pham <dat20036@gmail.com>
2022-03-08 10:57:00 +0700

    Disable null

--- Lilia/Commons/JsonManager.cs
+++ Lilia/Commons/JsonManager.cs
@@ -14,7 +14,7 @@ public static class JsonManager<T> where T : BaseJson
         if (File.Exists(filePath)) return;
         if (!createIfNotExist) return;
 
-        File.Create(filePath);
+        File.Create(filePath!);
         File.WriteAllText(filePath, JsonConvert.SerializeObject(typeof(T), Formatting.Indented));
         Log.Logger.Warning($"Created JSON file with path \"{filePath}\" since one doesn't exist");
     }
@@ -39,8 +39,7 @@ public static class JsonManager<T> where T : BaseJson
         }
         catch (NullReferenceException)
         {
-            Log.Logger.Fatal(
-                "Error in reading JSON, maybe you did not set a constructor with FilePath field overridden");
+            Log.Logger.Fatal("Error in reading JSON, maybe you did not set a constructor with FilePath field overridden");
             throw;
         }
     }
