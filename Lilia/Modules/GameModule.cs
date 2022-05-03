7e32b3f8aa920b23fd27081484784f65ec3a3e34
Tien Dat Pham <dat20036@gmail.com>
2022-05-03 16:05:56 +0700

    Add a new command

--- Lilia/Modules/GameModule.cs
+++ Lilia/Modules/GameModule.cs
@@ -6,7 +6,7 @@ namespace Lilia.Modules;
 
 public class GameModule : InteractionModuleBase<ShardedInteractionContext>
 {
-	[SlashCommand("coinflip", "Flip the coin")]
+	[SlashCommand("flip", "Flip the coin")]
 	public async Task GameCoinflipCommand()
 	{
 		await Context.Interaction.DeferAsync();
@@ -16,5 +16,18 @@ public class GameModule : InteractionModuleBase<ShardedInteractionContext>
 
 		await Context.Interaction.ModifyOriginalResponseAsync(x =>
 			x.Content = $"The coin turned {r[t]}");
+	}
+
+	[SlashCommand("pick", "Pick a choice from given pool of choices")]
+	public async Task GamePickCommand(
+		[Summary("choices", "List of choices, separated by '|'")] string pool)
+	{
+		await Context.Interaction.DeferAsync();
+
+		var choices = pool.Split('|', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+		var index = Random.Shared.Next(0, choices.Length - 1);
+
+		await Context.Interaction.ModifyOriginalResponseAsync(x =>
+			x.Content = $"I would pick: {choices[index]}");
 	}
 }
